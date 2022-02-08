67dacd6dfbf037ee09ef1740724491fc47f269dc
Júlio César Rodrigues Piau <39218054+juliocesarrodriguespiau@users.noreply.github.com>
2022-02-08 17:56:56 -0300

    ajuste no select

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php
 // cabecalhos obrigatórios
 header("Access-Control-Allow-Origin: *");
-
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 //header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-
 header("Content-Type: application/json; charset=UTF-8");
 
 //incluir conexao
 include_once 'conexao.php';
 
-//$query = "SELECT id, nome, email, id_vendedor, descricao_venda, comissao, valor_venda, data_venda FROM vendas ORDER BY id";
 $query = "SELECT * FROM vendas INNER JOIN tbl_vendedor ON vendas.id_vendedor = tbl_vendedor.id_vendedor;";
 $result = $conn->prepare($query);
 $result->execute();
 
 if(($result) AND ($result->rowCount() != 0)) {
     while($row_venda = $result->fetch(PDO::FETCH_ASSOC)) {
-        //var_dump($row_vendedor);
+        //echo($row_vendedor);
         extract($row_venda);
 
         $lista_vendas["records"][$id] = [
@@ -31,11 +28,10 @@ if(($result) AND ($result->rowCount() != 0)) {
             'data_venda' => $data_venda
         ];
     }
+
     //reposta com status 200
     http_response_code(200);
 
-    //retornar os vendedores em json
+    //retornar as vendas em json
     echo json_encode($lista_vendas);
-
 }
-
